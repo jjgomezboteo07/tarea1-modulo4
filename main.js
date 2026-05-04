a131fe34a6c55965d651b3cd24e2ccee8dcb83e0
Josue Gomez <josue.boteo.privados89@clases.edu.sv>
2026-05-03 21:31:16 -0600

    tarea 1 modulo 4

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const titulo = document.getElementById('titulo');
+
+const formulario = document.getElementById('Miformulario');
+formulario.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const emailUsuario = document.getElementById('email').value;
+
+    alert(`¡Gracias! Tu consulta ha sido enviada con éxito. Pronto recibirás una respuesta en: ${emailUsuario}`);
+
+    // 5. Opcional: Limpiar el formulario después de enviar
+    formulario.reset();
+});
